86d8eb355822976f48bb138dacea86d04c744ad9
saiflakhani <saiflakhani@gmail.com>
2020-05-09 15:48:29 +0530

    Initial Commit

---
+++ Assets/GameManager1.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager1 : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public float delayRestart= 1f;
+    // Start is called before the first frame update
+    public void EndGame()
+    {
+        if(!gameHasEnded){
+            Debug.Log("GAME OVER");
+            gameHasEnded = true;
+            Invoke("Restart", delayRestart);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public hmmscript movement;
+    
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision info)
+    {
+        if(info.collider.tag == "Obstacle"){
+            Debug.Log("We hit an obstacle");
+            movement.enabled = false;
+            FindObjectOfType<GameManager1>().EndGame();
+        }
+    }
+}
